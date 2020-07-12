8b33910eeca7d83719e5e1383d2bc4ba73ae78a1
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-12 03:24:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,17 @@
+phrase = "I LOVE YOU GRANDMA!"
+
+def speak_to_grandma(phrase)
+    
+if phrase == phrase.downcase
+  print "HUH?! SPEAK UP, SUNNY!"
+elsif phrase == "I LOVE YOU GRANDMA!"
+  print "I LOVE YOU TOO PUMPKIN!"
+else
+  print "NO, NOT SINCE 1938!"
+end
+end
+
+speak_to_grandma(phrase)
 
 
   
